e6cdc978859563a5c6c1bbacb83da46682eda74c
Vichukano <vichukano@gmail.com>
2021-09-25 14:05:33 +0300

    fix client close, now it works

--- src/main/java/ru/vichukano/ignite/server/IgniteServer.java
+++ src/main/java/ru/vichukano/ignite/server/IgniteServer.java
@@ -4,8 +4,8 @@ import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.query.ContinuousQuery;
 import org.apache.ignite.client.ClientCache;
+import org.apache.ignite.client.ClientCacheConfiguration;
 import org.apache.ignite.client.IgniteClient;
-import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.ClientConfiguration;
 
 import javax.cache.configuration.Factory;
@@ -13,7 +13,8 @@ import javax.cache.event.CacheEntryEventFilter;
 import javax.cache.event.EventType;
 import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
-import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class IgniteServer {
     private static final String CACHE_NAME = "ignite listener";
@@ -21,16 +22,15 @@ public class IgniteServer {
     public static void main(String[] args) throws Exception {
         try (Ignite ignite = Ignition.start("src/main/resources/ignite.xml")) {
             System.out.println("-----------IGNITE SERVER STARTED---------------");
-            CacheConfiguration<Integer, String> cc = new CacheConfiguration<>();
-            cc.setName(CACHE_NAME).setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(new Duration(TimeUnit.SECONDS, 2)));
-            cc.setEagerTtl(true);
-            cc.setStatisticsEnabled(true);
-            ignite.createCache(cc);
-            System.out.println("-----------IGNITE CACHE CREATED---------------");
             ClientConfiguration cfg = new ClientConfiguration().setAddresses("127.0.0.1:10800");
+            ClientCacheConfiguration ccc = new ClientCacheConfiguration()
+                    .setName(CACHE_NAME)
+                    .setEagerTtl(true)
+                    .setStatisticsEnabled(true)
+                    .setExpiryPolicy(CreatedExpiryPolicy.factoryOf(new Duration(SECONDS, 2)).create());
             try (IgniteClient client = Ignition.startClient(cfg)) {
                 System.out.println("-----------IGNITE THIN CLIENT STARTED---------------");
-                ClientCache<Integer, String> cache = client.getOrCreateCache(CACHE_NAME);
+                ClientCache<Integer, String> cache = client.getOrCreateCache(ccc);
                 ContinuousQuery<Integer, String> query = new ContinuousQuery<>();
                 query.setIncludeExpired(true);
                 query.setLocalListener(events -> events.forEach(e -> System.out.println(
@@ -41,15 +41,15 @@ public class IgniteServer {
                                 + " "
                                 + e.getValue())));
                 //Filter fo expired entity
-                query.setRemoteFilterFactory((Factory<CacheEntryEventFilter<Integer, String>>) () -> e -> true);
+                query.setRemoteFilterFactory((Factory<CacheEntryEventFilter<Integer, String>>) () -> e -> e.getEventType() == EventType.EXPIRED);
                 cache.query(query);
                 System.out.println("-----------INSERT---------------");
                 cache.put(1, "one");
                 cache.put(2, "two");
                 cache.put(3, "three");
                 System.out.println("-----------CACHE POPULATED---------------");
+                Thread.sleep(10000);
             }
-            Thread.sleep(20000);
         }
     }
 }
